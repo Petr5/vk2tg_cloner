838e1fdb0812b334d0b0f8772f8a487942546bdf
freQuensy <mamaetevalex@gmail.com>
2022-07-10 01:06:35 +0300

    Вычленение токена из длинной ссылки

---
+++ utils.py
@@ -0,0 +1,3 @@
+def get_token_from_url(url: str) -> str:
+    """get url like this https://oauth.vk.com/blank.html#access_token=vk1.a.2Y6SAaTg9dZUipXtWcGtCToXxA452AVIWPsz0EEtNSC3ePRvVP6qcxwjh1cEIImcAtM_RKMPoDyX8Ma5Q5_2ke9ztWbyWBSAFdE0GBP3VhrUd5qvxutG1igvbFHdIbkwWV9DDgTGqDNS9qkITeT3ouGEqju9ZQe_NlqszVVnQhxy6anJtQhvYOmSZfO6TUag&expires_in=0&user_id=536270471 and convert it to access token"""
+    return url.split("access_token=")[1].split("&")[0]
